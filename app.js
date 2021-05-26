9fcd57f3d6c455952539278c7e51ad72cae0765b
Anjana Bandara <anjanabandara@Anjanas-MacBook-Air.local>
2021-05-26 20:29:43 +1000

    base assignment

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data
-d3.csv("data/data.csv").then(function(acsData) {
+d3.csv("./data.csv").then(function(acsData) {
   console.log(acsData);
     // Step 1: Parse Data/Cast as numbers
     // ==============================
@@ -33,11 +33,11 @@ d3.csv("data/data.csv").then(function(acsData) {
     // Step 2: Create scale functions
     // ==============================
   var xScale = d3.scaleLinear()
-    .domain([0, d3.max(acsData, d => d.poverty)])
+    .domain([d3.min(acsData, d => d.poverty) -2, d3.max(acsData, d => d.poverty) +2])
     .range([0, width]);
 
   var yScale = d3.scaleLinear()
-    .domain([0, d3.max(acsData => d.healthcare)])
+    .domain([d3.min(acsData, d => d.healthcare) -1, d3.max(acsData, d => d.healthcare) +1])
     .range([height, 0]);
     // Step 3: Create axis functions
     // ==============================
@@ -55,22 +55,22 @@ d3.csv("data/data.csv").then(function(acsData) {
     // Step 5: Create Circles
     // ==============================
   var circlesGroup = chartGroup.selectAll("circle")
-    .data(hairData)
+    .data(acsData)
     .enter()
     .append("circle")
     .attr("cx", d => xScale(d.poverty))
-    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("cy", d => yScale(d.healthcare))
     .attr("r", "10")
-    .attr("fill", "gold")
-    .attr("stroke-width", "1")
+    .attr("fill", "pink")
+    .attr("class", "stateCircle")
     .attr("stroke", "black");
     // Step 6: Initialize tool tip
     // ==============================
     var toolTip = d3.tip()
-    .attr("class", "tooltip")
+    .attr("class", "d3-tip")
     .offset([80, -60])
     .html(function(d) {
-      return (`healthcare <strong>${(d.healthcare)}<strong><hr>${d.poverty}`);
+      return (`healthcare ${(d.healthcare)}<hr>poverty rate ${d.poverty}`);
     });
 
     // Step 7: Create tooltip in the chart
@@ -87,17 +87,29 @@ d3.csv("data/data.csv").then(function(acsData) {
         toolTip.hide(d);
       });
     // Create axes labels
+
+    chartGroup.append("text")
+	    .attr("class", "stateText")
+      .selectAll("tspan")
+      .data(acsData)
+      .enter()
+      .append("tspan")
+      .attr("x", d => xScale(d.poverty))
+      .attr("y", d => yScale(d.healthcare))
+	    .text(function(d){return d.abbr})
+
+
     chartGroup.append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 0 - margin.left + 40)
       .attr("x", 0 - (height / 2))
       .attr("dy", "1em")
-      .attr("class", "axisText")
+      .attr("class", "aText")
       .text("Lacks Healthcare%");
 
     chartGroup.append("text")
       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-      .attr("class", "axisText")
+      .attr("class", "aText")
       .text("In poverty(%)");
   }).catch(function(error) {
     console.log(error);
